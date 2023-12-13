fc3cea1d19530d3feaea0a25095fb096f38a299c
Beatriz-Munoz-Abellan <beatrizmunozabellan@gmail.com>
2023-12-13 17:21:08 +0100

    ee

--- js/script.js
+++ js/script.js
@@ -1,33 +1,44 @@
- // Función para cambiar el idioma
- function changeLanguage(lang) {
-    // Utiliza fetch para cargar el contenido desde el archivo JSON
-    fetch('./json/texto.json')
-      .then(response => response.json())
-      .then(data => {
-        // Selecciona el idioma deseado
-        const aboutContent = data.about.idioma[lang];
+// Función para cambiar el idioma
+function changeLanguage(lang) {
+  // Utiliza fetch para cargar el contenido desde el archivo JSON
+  fetch('./json/texto.json')
+    .then(response => response.json())
+    .then(data => {
+      // Selecciona el idioma deseado para la sección "about"
+      const aboutContent = data.about.idioma[lang];
+      // Mostrar el contenido en el elemento con id "cont-about"
+      document.getElementById("cont-about").innerText = aboutContent;
 
-        // Mostrar el contenido en el elemento con id "cont-about"
-        document.getElementById("cont-about").innerText = aboutContent;
-      })
-      .catch(error => console.error('Error al cargar el archivo JSON:', error));
-  }
+      // Selecciona el idioma deseado para la sección "expe"
+      const expeContent = data.expe[lang];
 
-  // Carga el contenido predeterminado (ES)
-  changeLanguage('es');
+      // Obtén el contenedor donde se mostrará la información de experiencia
+      const contExpe = document.getElementById("cont-expe");
 
-  document.addEventListener("DOMContentLoaded", function() {
-    // Crear el elemento del efecto de iluminación
-    var mouseGlow = document.createElement("div");
-    mouseGlow.id = "mouse-glow";
-    document.body.appendChild(mouseGlow);
+      // Limpiar el contenido existente
+      contExpe.innerHTML = "";
 
-    // Seguir la posición del ratón y aplicar el efecto
-    document.addEventListener("mousemove", function(event) {
-        var x = event.clientX;
-        var y = event.clientY;
+      // Iterar sobre la información de experiencia y agregarla al contenedor
+      expeContent.forEach(experience => {
+        // Crear una cadena HTML para tags con espacios entre las etiquetas
+        const tagsHTML = experience.tags.map(tag => `<span class="tags">${tag}</span>`).join(' ');
 
-        mouseGlow.style.left = x + "px";
-        mouseGlow.style.top = y + "px";
-    });
-});
+        // Crear la estructura HTML
+        const htmlContent = `
+          <div class="col">
+            <div class="cont">
+              <span class="date">${experience.date}</span>
+              <h3 class="title">${experience.title}</h3>
+              <p class="description">${experience.description}<br>${tagsHTML}</p>
+            </div>
+          </div>
+        `;
+        // Agregar el contenido al contenedor
+        contExpe.innerHTML += htmlContent;
+      });
+    })
+    .catch(error => console.error('Error al cargar el archivo JSON:', error));
+}
+
+// Carga el contenido predeterminado (ES)
+changeLanguage('es');
